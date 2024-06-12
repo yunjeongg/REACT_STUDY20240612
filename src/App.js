c5b6f7e9500e6c38ea145b4c6bf4860cff14f1dd
yunjeongg <bae.yunjeongg@gmail.com>
2024-06-12 11:33:33 +0900

    # 05. props 02

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react'
-import ExpenseItem from './components/expenses/ExpenseItem';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import ExpenseItem from "./components/expenses/ExpenseItem";
 
-// Props 
+// Props
 // 컴포넌트 간에 데이터를 전달하는 메커니즘
 // 단방향데이터흐름을 지원하는데 부모 -> 자식 컴포넌트로 전달된다.
 // 속성에 값을 넣어주고, componenet 함수의 파라미터로 받을 수 있다.
 // 속성을 보낼 때 반드시 {} 를 붙여야 하지만 문자열만 생략가능
 const App = () => {
+  // 서버에서 지출항목 JSON 배열을 응답받았다고 가정했을 때
+  const expenses = 
+  [
+    // 날씨의 달은 -1 해줘야 한다. 스크립트 자체문제
+    {
+      title: '국밥먹음',
+      price: 10000,
+      date: new Date(2024, 6 - 1, 3)
+    }, 
+    {
+      title: '치킨찍찍',
+      price: 35000,
+      date: new Date(2024, 3 - 1, 18)
+    }, 
+    {
+      title: '돈가스쩝쩝',
+      price: 12000,
+      date: new Date(2024, 1 - 1, 22)
+    }
+  ]
   return (
     <>
-    <ExpenseItem title="국밥먹음" price={8000} date={new Date(2024, 6, 3)}/>
-    <ExpenseItem title="치킨배달" price={30000} date={new Date(2024, 1, 18)}/>
-    <ExpenseItem title="돈가스냠냠" price={10000} date={new Date(2024, 3, 23)}/>
+      <ExpenseItem 
+        title={expenses[0].title}
+        price={expenses[0].price}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        price={expenses[1].price}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        price={expenses[2].price}
+        date={expenses[2].date}
+      />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -1,13 +1,14 @@
-import React from 'react'
-import './ExpenseItem.css';
+import React from "react";
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
-// Props 
+// Props
 // 파라미터로 받을 때 두 가지 방법이 있다.
 // 1.
 // const ExpenseItem = (aaa) => {
-  
+
 // 2. aaa. 안붙이고싶을 때
-const ExpenseItem = ({date, title, price: exPrice }) => {
+const ExpenseItem = ({ date, title, price: exPrice }) => {
   // console.log('props: ', aaa);
 
   // 변수 선언
@@ -19,11 +20,10 @@ const ExpenseItem = ({date, title, price: exPrice }) => {
 
   // 1자리 숫자를 2자리수로 변환하는 함수
   const make2digit = (text) => {
-    return text.toString().padStart(2, '0');
+    return text.toString().padStart(2, "0");
   };
 
   const makeFormattedDate = () => {
-
     const year = date.getFullYear();
     const month = date.getMonth();
     const day = date.getDate();
@@ -32,17 +32,20 @@ const ExpenseItem = ({date, title, price: exPrice }) => {
   };
 
   // 원화 표기법으로 변환
-  const formattedPrice = new Intl.NumberFormat('ko-KR').format(exPrice);
+  const formattedPrice = new Intl.NumberFormat("ko-KR").format(exPrice);
 
+  // 아래 <ExpenseDate exDate={date} />
+  // date 를 exDate 이름으로 ExpenseDate.js 에 전달하겠다는 뜻.
+  
   return (
-    <div className='expense-item'>
-      <div>{makeFormattedDate()}</div>
-      <div className='expense-item__description'>
+    <div className="expense-item">
+      <ExpenseDate exDate={date} />
+      <div className="expense-item__description">
         <h2>{title}</h2>
-        <div className='expense-item__price'>{formattedPrice}원</div>
+        <div className="expense-item__price">{formattedPrice}원</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ExpenseItem
+export default ExpenseItem;
