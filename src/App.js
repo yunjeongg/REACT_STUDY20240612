9bce02e80a38a1debde740b7d40b058def16a4a3
yunjeongg <bae.yunjeongg@gmail.com>
2024-06-14 11:34:54 +0900

    # 19. 기본저장 (나의 목표 추가하기)

--- src/App.js
+++ src/App.js
@@ -1,60 +1,18 @@
-import logo from "./logo.svg";
-import "./App.css";
-import React, { useState } from "react";
-import ExpenseList from "./components/expenses/ExpenseList";
-import Greet from "./components/Greet";
-import Counter from "./components/practice/Counter";
-import NewExpense from "./components/new-expenses/NewExpense";
-import CheckBoxStyle from "./components/practice/CheckBoxStyle";
-
-// Props Children
-// 적용할 컴포넌트 태그들의 오프닝, 클로징태그로 children 태그를 넣어주고, 
-// 속성과 값을 넣어주면
-// children 태그 컴포넌트에서 파라미터로 받아주면 접근할 수 있다.
+import React, { useState } from 'react';
+import CourseInput from './components/CourseGoals/CourseInput';
+import CourseList from './components/CourseGoals/CourseList';
+import './App.css';
 
 const App = () => {
-  // 서버에서 지출항목 JSON 배열을 응답받았다고 가정했을 때
-  const expenses = 
-  [
-    // 날씨의 달은 -1 해줘야 한다. 스크립트 자체문제
-    {
-      title: '국밥먹음',
-      price: 10000,
-      date: new Date(2024, 6 - 1, 3)
-    }, 
-    {
-      title: '치킨찍찍',
-      price: 35000,
-      date: new Date(2023, 3 - 1, 18)
-    }, 
-    {
-      title: '돈가스쩝쩝',
-      price: 12000,
-      date: new Date(2021, 1 - 1, 22)
-    },
-    {
-      title: '파파피자',
-      price: 40000,
-      date: new Date(2022, 10 - 1, 2)
-    },
-    {
-      title: '엄마손칼국수',
-      price: 8000,
-      date: new Date(2020, 12 - 1, 25)
-    }
-  ];
-
-  // 배열을 상태변수로 관리
-  const [expenseList, setExpenseList] = useState(expenses); 
-
-  // ExpenseForm에게 내려보낼 함수
-  const onAddExpense = (userInput) => setExpenseList([...expenseList, userInput]);
-
   return (
-    <>
-      <NewExpense onSave={onAddExpense} />
-      <ExpenseList expenses={expenseList} />
-    </>
+    <div>
+      <section id="goal-form">
+        <CourseInput />
+      </section>
+      <section id="goals">
+        <CourseList />
+      </section>
+    </div>
   );
 };
 
---
+++ src/components/CourseGoals/CourseList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './CourseList.css';
+
+const CourseList = () => {
+  return (
+    <ul className='goal-list'></ul>
+  );
+};
+
+export default CourseList;
