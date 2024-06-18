4ad0ab6ffd7af53e18525185c621d312ed5cc479
yunjeongg <bae.yunjeongg@gmail.com>
2024-06-18 10:04:52 +0900

    # 37. useEffect 기초

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 import MainHeader from './components/SideEffect/MainHeader';
 import Home from './components/SideEffect/Home';
@@ -8,18 +8,31 @@ import Login from './components/SideEffect/Login';
 const App = () => {
 
   // 현재 로그인 상태를 체크하는 변수
-  const [isLoggedIn, setIsloggedIn] = useState(false); // 처음은 로그인 안한 상태 false
+  const [isLoggedIn, setIsloggedIn] = useState(false);
 
-  console.log("로그인 검사 수행");
+  // console.log('로그인 검사 수행!');
+  // localStorage에서 login-flag를 꺼냄
+  // const storedLoginFlag = localStorage.getItem('login-flag');
+  // 로그인 검사를 초기에 1번만 수행
+  // if (storedLoginFlag === '1') {
+    // 상태변수가 setter로 변경되면 
+    // 리액트는 변경감지 후 바로 리렌더링을 수행함
+    // setIsloggedIn(true);
+  // }
 
-  // localStorage 에서 login-flag 를 꺼내기
-  const storedLoginFlag = localStorage.getItem('login-flag');
+  // side effect 처리를 위한 함수
+  // useEffect 는 기본적으로 컴포넌트 랜더링 시 단 한번만 호출된다.
+  useEffect(() => {
 
-  // 로그인 검사를 초기에 한번만 수행
-  if (storedLoginFlag == '1') {
-    // 상태변수가 setter 로 변경되면 리액트는 변경감지 후 바로 리랜더링을 수행한다.
-    setIsloggedIn(true);
-  }
+    // useEffect 내부에서는 Dom을 가져올 수 있다.
+    // ex) document.getElementById(...)
+
+    console.log('로그인 검사 수행!');
+    const storedLoginFlag = localStorage.getItem('login-flag');
+    if (storedLoginFlag === '1') {
+      setIsloggedIn(true);
+    }
+  }, []);
 
   // 서버통신은 중앙집중 관리가 중요하다.
   // 로그인을 위한 요청이라 로그인 컨포넌트에서 만들수도 있지만
@@ -34,9 +47,14 @@ const App = () => {
     setIsloggedIn(true); // 로그인 한 상태로 바꿔주기
   };
 
+  const logoutHandler = () => {
+    localStorage.removeItem('login-flag');
+    setIsloggedIn(false);
+  };
+
   return (
     <>
-      <MainHeader />
+      <MainHeader onLogout={logoutHandler} />
       <main>
         {/* 로그인이 된 상태면 <Home />을 보여주고 */}
         { isLoggedIn && <Home />}
