21d120f444351c6d78a86fe16d0e427de1e17f2f
yunjeongg <bae.yunjeongg@gmail.com>
2024-06-18 09:30:31 +0900

    # 35. 로그인 기능 동작 주석 추가

--- src/components/SideEffect/Login.js
+++ src/components/SideEffect/Login.js
@@ -5,15 +5,23 @@ import styles from './Login.module.css';
 import Button from '../UI/Button';
 
 const Login = ({ onLogin }) => {
+  // 사용자가 입력한 이메일을 상태관리
   const [enteredEmail, setEnteredEmail] = useState('');
+  // 이메일 입력값이 정상인지 유무 확인 (true & false)
   const [emailIsValid, setEmailIsValid] = useState();
+  // 사용자가 입력한 패스워드를 상태관리
   const [enteredPassword, setEnteredPassword] = useState('');
+  // 패스워드 입력값이 정상인지 유무 확인
   const [passwordIsValid, setPasswordIsValid] = useState();
+
+  // 이메일, 패스워드가 둘 다 정상인지 확인
   const [formIsValid, setFormIsValid] = useState(false);
 
+  // 이메일을 입력할 때 마다 입력값이 등록
   const emailChangeHandler = (e) => {
     setEnteredEmail(e.target.value);
 
+    // 이메일에 @ 가 포함되어있고, 비밀번호가 좌우공백제외 6글자 이상
     setFormIsValid(
       e.target.value.includes('@') && enteredPassword.trim().length > 6
     );
@@ -35,6 +43,7 @@ const Login = ({ onLogin }) => {
     setPasswordIsValid(enteredPassword.trim().length > 6);
   };
 
+  // 로그인 버튼 눌렀을 때 onLogin 함수에 두 파라미터 전달
   const submitHandler = (e) => {
     e.preventDefault();
     onLogin(enteredEmail, enteredPassword);
@@ -44,7 +53,7 @@ const Login = ({ onLogin }) => {
     <Card className={styles.login}>
       <form onSubmit={submitHandler}>
         <div
-          className={`${styles.control} ${
+          className={`${styles.control} ${ // 이메일 입력이 제대로 안됐을 경우 스타일 추가
             emailIsValid === false ? styles.invalid : ''
           }`}
         >
@@ -53,6 +62,8 @@ const Login = ({ onLogin }) => {
             type="email"
             id="email"
             value={enteredEmail}
+            // 포커스 이벤트 = 커서를 얻었을 때
+            // 블러이벤트 = 커서를 잃었을 때
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
@@ -72,7 +83,9 @@ const Login = ({ onLogin }) => {
           />
         </div>
         <div className={styles.actions}>
-          <Button type="submit" className={styles.btn} disabled={!formIsValid}>
+          <Button type="submit" 
+                  className={styles.btn} 
+                  disabled={!formIsValid}>
             Login
           </Button>
         </div>
--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import styles from './Button.module.css';
 
-const Button = ({ type, onClick, children}) => {
+const Button = ({ type, onClick, className, disabled, children }) => {
+  // {type || 'button'} = type 이 들어오면 type, 안들어오면 button 타입
   return (
-    <button type={type} className={styles.button} onClick={onClick}>
+    <button
+      type={type || 'button'}
+      className={`${styles.button} ${className}`}
+      onClick={onClick}
+      disabled={disabled}
+    >
       {children}
     </button>
   );
