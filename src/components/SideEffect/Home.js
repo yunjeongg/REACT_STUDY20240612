d8083de65be27fc267b40ddf6cd0b5aa6ae1b497
yunjeongg <bae.yunjeongg@gmail.com>
2024-06-19 09:52:34 +0900

    # 43. useContext Hook

--- src/components/SideEffect/Home.js
+++ src/components/SideEffect/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import Card from '../UI/Card';
 import styles from './Home.module.css';
@@ -6,21 +6,17 @@ import Button from '../UI/Button';
 import AuthContext from '../../store/auth-context';
 
 const Home = () => {
+
+   const {onLogout} = useContext(AuthContext);
+  //  console.log('ctx: ', ctx);
+
   return (
-    <AuthContext.Consumer>
-      {
-        (ctx) => {
-          console.log('ctx: ', ctx);
-          return (
-            <Card className={styles.home}>
-              <h1>Welcome back!</h1>
-              <Button onClick={ctx.onLogout}>Logout</Button>
-            </Card>
-          )
-        }
-      }
-    </AuthContext.Consumer>
+    <Card className={styles.home}>
+      <h1>Welcome back!</h1>
+      <Button onClick={onLogout}>Logout</Button>
+    </Card>
   );
+
 };
 
 export default Home;
