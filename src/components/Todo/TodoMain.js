c6d539f06287d709052e21fe0197e1b1677efdda
yunjeongg <bae.yunjeongg@gmail.com>
2024-06-20 09:29:15 +0900

    # 48.  (To do List 03_2)  할 일 추가

--- src/components/Todo/TodoMain.js
+++ src/components/Todo/TodoMain.js
@@ -3,18 +3,11 @@ import React from 'react';
 import './scss/TodoMain.scss';
 import TodoItem from "./TodoItem";
 
-const DUMMY_TODOS = [
-  { id: 1, title: '리액트 공부', done: true },
-  { id: 2, title: '점심 먹기', done: false },
-  { id: 3, title: '프로젝트하기', done: false },
-  { id: 4, title: '숙제하기', done: false },
-];
-
-const TodoMain = () => {
+const TodoMain = ({ todos }) => {
   return (
     <ul className='todo-list'>
       {
-        DUMMY_TODOS.map(todo => <TodoItem key={todo.id} item={todo} />)
+        todos.map(todo => <TodoItem key={todo.id} item={todo} />)
       }
     </ul>
   );
--- src/components/Todo/TodoTemplate.js
+++ src/components/Todo/TodoTemplate.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import './scss/TodoTemplate.scss';
 import TodoHeader from "./TodoHeader";
 import TodoInput from "./TodoInput";
@@ -6,10 +6,24 @@ import TodoMain from "./TodoMain";
 
 
 const TodoTemplate = () => {
+
+  const [inputDates, setInputDate] = useState([]);
+
+  const DUMMY_TODOS = [
+    { id: 1, title: '리액트 공부', done: true },
+    { id: 2, title: '점심 먹기', done: false },
+    { id: 3, title: '프로젝트하기', done: false },
+    { id: 4, title: '숙제하기', done: false },
+  ];
+
+  const addInputHandler = (inputDateObject) => {
+    console.log("값", inputDateObject);
+  }
+
   return (
     <div className='TodoTemplate'>
       <TodoHeader />
-      <TodoMain />
+      <TodoMain todos={DUMMY_TODOS} />
       <TodoInput />
     </div>
   );
