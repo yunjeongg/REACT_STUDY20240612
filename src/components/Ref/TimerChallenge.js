4baa0581215ad41e21b3faa8974be9a229af274b
yunjeongg <bae.yunjeongg@gmail.com>
2024-06-20 11:03:04 +0900

    # 58. 타이머 Stop 안되는 문제 01

--- src/components/Ref/TimerChallenge.js
+++ src/components/Ref/TimerChallenge.js
@@ -8,14 +8,31 @@ const TimerChallenge = ({ title, targetTime }) => {
   // 타겟시간이 종료되었는지 여부
   const [timerExpired, setTimerExpired] = useState(false);
 
+  let timer; // setTimeout 의 참조변수
+
   const startHandler = e => {
 
-    setTimeout(() => {
+    // 1. 지역변수 timer 저장
+    timer = setTimeout(() => {
       setTimerExpired(true);
     }, targetTime * 1000);
 
+    // 2. 리랜더링
     setTimerStarted(true);
   };
+
+  // 중지할 때 실행할 코드
+  const stopHandler = e => {
+    console.log("stop");
+
+    // 3. 서로 다른변수
+    clearTimeout(timer);
+
+    // 이 상황에 stop이 작동하지 않는 이유는 
+    // start시의 timer변수가 지역변수이기 때문에 
+    // 상태변수의 setter호출시 리랜더링이 되면서 새로운 지역변수로 바뀜
+    // stop시의 timer와 start시의 timer는 다른 변수다.
+  }
 
   return (
     <section className="challenge">
@@ -25,7 +42,7 @@ const TimerChallenge = ({ title, targetTime }) => {
         {targetTime} second{targetTime > 1 ? 's' : ''}
       </p>
       <p>
-        <button onClick={startHandler}>
+        <button onClick={timerStarted ? stopHandler : startHandler}>
           {timerStarted ? 'Stop' : 'Start'} Challenge
         </button>
       </p>
