6fc8f510ec6a603cde275d8144d75dd4898db5b1
yunjeongg <bae.yunjeongg@gmail.com>
2024-06-20 09:21:09 +0900

    # 47. (To do List 02)  할 일 목록 동적 랜더링하기

--- src/components/Todo/TodoMain.js
+++ src/components/Todo/TodoMain.js
@@ -3,12 +3,19 @@ import React from 'react';
 import './scss/TodoMain.scss';
 import TodoItem from "./TodoItem";
 
+const DUMMY_TODOS = [
+  { id: 1, title: '리액트 공부', done: true },
+  { id: 2, title: '점심 먹기', done: false },
+  { id: 3, title: '프로젝트하기', done: false },
+  { id: 4, title: '숙제하기', done: false },
+];
+
 const TodoMain = () => {
   return (
     <ul className='todo-list'>
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
+      {
+        DUMMY_TODOS.map(todo => <TodoItem key={todo.id} item={todo} />)
+      }
     </ul>
   );
 };
