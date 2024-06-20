15e254ed2ace33eb81cbd7ee0bfa81c59fd5f972
yunjeongg <bae.yunjeongg@gmail.com>
2024-06-20 09:49:12 +0900

    # 48.  (To do List 03_3)  할 일 추가

--- src/components/Todo/TodoInput.js
+++ src/components/Todo/TodoInput.js
@@ -1,21 +1,41 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { MdAdd } from 'react-icons/md';
 
 import './scss/TodoInput.scss';
 
-const TodoInput = () => {
+const TodoInput = ({ onAdd }) => {
+
+  // input의 주소값을 기억하는 변수 생성
+  // useRef 는 값이 변경되도 랜더링에 영향을 주지 않는다.
+  // 또는 Dom 을 제어할 때 사용한다.
+  const $textInput = useRef();
+
   // 입력창 토글링 상태값
   const [open, setOpen] = useState(false);
 
   // 버튼 토글링 함수
   const onToggle = () => setOpen(prevOpen => !prevOpen);
 
+  const submitHandler = e => {
+    e.preventDefault();
+    onAdd($textInput.current.value);
+    // form이 제출되면 입력창 비우기
+
+    // useRef 안의 값은 무조건 .current 을 사용하기
+    // $textInput.current 와 document.quertSelector('input') 는 같다.
+    // console.log($textInput.current.value);
+    
+    $textInput.current.value = '';
+    setOpen(false);
+  };
+
   return (
     <>
       {open && (
         <div className="form-wrapper">
-          <form className="insert-form">
+          <form className="insert-form" onSubmit={submitHandler}>
             <input
+              ref={$textInput}
               type="text"
               placeholder="할 일을 입력 후, 엔터를 누르세요!"
             />
--- src/components/Todo/TodoTemplate.js
+++ src/components/Todo/TodoTemplate.js
@@ -4,27 +4,43 @@ import TodoHeader from "./TodoHeader";
 import TodoInput from "./TodoInput";
 import TodoMain from "./TodoMain";
 
+const DUMMY_TODOS = [
+  { id: 1, title: '리액트 공부', done: true },
+  { id: 2, title: '점심 먹기', done: false },
+  { id: 3, title: '프로젝트하기', done: false },
+  { id: 4, title: '숙제하기', done: false },
+];
 
 const TodoTemplate = () => {
 
-  const [inputDates, setInputDate] = useState([]);
+  const [todoList, setTodoList] = useState(DUMMY_TODOS);
 
-  const DUMMY_TODOS = [
-    { id: 1, title: '리액트 공부', done: true },
-    { id: 2, title: '점심 먹기', done: false },
-    { id: 3, title: '프로젝트하기', done: false },
-    { id: 4, title: '숙제하기', done: false },
-  ];
+  const makeNewId = () => {
+    // 데이터가 없을 경우 id 는 1
+    if (todoList.length === 0) return 1;
+    // 그렇지 않을 경우 더미데이터 맨 끝 id 
+    return todoList[todoList.length - 1].id + 1;
+  };
 
-  const addInputHandler = (inputDateObject) => {
-    console.log("값", inputDateObject);
-  }
+  // 데이터 상향식 전달을 위한 함수를 생성 
+  // (TodoInput에서 할 일을 끌어올리는 역할)
+  const addTodo = (newTitle) => {
+    const newTodo = {
+      id: makeNewId(),
+      title: newTitle,
+      done: false
+    };
+
+    // console.log('new: ', newTodo);
+    
+    setTodoList(prevTodoList => [...prevTodoList, newTodo]);
+  };
 
   return (
     <div className='TodoTemplate'>
       <TodoHeader />
-      <TodoMain todos={DUMMY_TODOS} />
-      <TodoInput />
+      <TodoMain todos={todoList} />
+      <TodoInput onAdd={addTodo} />
     </div>
   );
 };
