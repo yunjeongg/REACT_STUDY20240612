c13cf74bc8716ef3d3d94ffc6e5eb9173cd5f7d8
yunjeongg <bae.yunjeongg@gmail.com>
2024-06-20 10:37:29 +0900

    # 54. 입력창에 입력한 이름 적용하기

--- src/components/Ref/Player.js
+++ src/components/Ref/Player.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 const Player = () => {
+
+  const $nameInputRef = useRef();
+
+  const [enteredName, setEnteredName] = useState('anonymous');
+
+  const nameChangeHandler = e => {
+    console.log('click');
+
+    // 입력창에 입력한 내용이 Welcome 뒤에 들어가게하기
+    setEnteredName($nameInputRef.current.value);
+
+    // 입력창 비우기
+    $nameInputRef.current.value = '';
+  };
+
   return (
     <section id="player">
-      <h2>Welcome anonymous!</h2>
+      <h2>Welcome {enteredName}!</h2>
       <p>
-        <input type="text"/>
-        <button>Set Name</button>
+        <input ref={$nameInputRef} type="text"/>
+        <button onClick={nameChangeHandler}>Set Name</button>
       </p>
     </section>
   );
