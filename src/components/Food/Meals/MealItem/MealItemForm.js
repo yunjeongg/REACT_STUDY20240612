91a6fd931d493d1635c0899c385ff811c52d1323
yunjeongg <bae.yunjeongg@gmail.com>
2024-06-21 11:52:47 +0900

    # 74. forwardRef

--- src/components/Food/Meals/MealItem/MealItemForm.js
+++ src/components/Food/Meals/MealItem/MealItemForm.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import Input from '../../../UI/Input/Input';
 import styles from './MealItemForm.module.scss';
 
 const MealItemForm = (props) => {
+
+  // 선택한 수량값
+  // const [amount, setAmount] = useState(0);
+
+  // 선택한 수량 값 가져오기
+  const inputRef = useRef();
+
+  const submitHandler = e => {
+    e.preventDefault();
+    const seletedValue = inputRef.current.value;
+    console.log('select: ', seletedValue);
+  };
+
   return (
-    <form className={styles.form}>
-      <Input label='취미' inputAttr={{type: 'checkbox', checked: 'checked', class: 'abc', id: 'aaa'}} />
-      <Input
+    <form className={styles.form} onSubmit={submitHandler}>
+      {/* <Input label='취미' inputAttr={{type: 'checkbox', checked: 'checked', class: 'abc', id: 'aaa'}} /> */}
+      <input ref={inputRef} type='number' />
+      {/* <Input
+        ref={inputRef}
         label='수량'
         inputAttr={{
           id: 'amount_' + props.id,
@@ -16,7 +31,7 @@ const MealItemForm = (props) => {
           step: '1',
           defaultValue: '1',
         }}
-      />
+      /> */}
       <button>담기</button>
     </form>
   );
