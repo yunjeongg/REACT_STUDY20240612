2e90db48862056171cea8cc9fed63aef1d8cf1cd
yunjeongg <bae.yunjeongg@gmail.com>
2024-06-21 09:25:58 +0900

    # 66. css 파일 적용 & 동적이미지 업로드

---
+++ src/components/Food/Layout/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+// import 변수명 from .scss 스타일파일
+import styles from './Header.module.scss';
+
+// 정적 이미지 로딩
+// import 변수명 from 사진경로
+import foodImage from '../../../assets/img/meals.jpg'
+
+const Header = () => {
+  console.log(styles);
+
+  // 디스트럭처링
+  const { header, 'main-image':mainImage } = styles;
+  return (
+    <>
+      <header className={styles.header}>
+      {/* <header className='Header_header__u1k60'> */}
+        <h1>ReactMeals</h1>
+      </header>
+      <div className={mainImage}>
+        <img src={foodImage} alt='맛있어보이는음식' />
+      </div>
+    </>
+  )
+}
+
+export default Header
