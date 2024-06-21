9489ae1442ffe92db4b2102e802ff2dc133179ec
yunjeongg <bae.yunjeongg@gmail.com>
2024-06-21 12:00:02 +0900

    # 75. 장바구니에 보낼 객체데이터 만들기

--- src/components/Food/Meals/MealItem/MealItem.js
+++ src/components/Food/Meals/MealItem/MealItem.js
@@ -8,6 +8,17 @@ const MealItem = ({id, price, description, name}) => {
 
   const formatPrice = new Intl.NumberFormat("ko-KR").format(price);
 
+  // MealItemForm 에서 선택한 수량값을 끌어올려줄 함수
+  const addToCartHandler = (amount) => {
+    const item = {
+      id: id,
+      name: name,
+      amount: +amount, // 문자타입을 숫자타입으로 변환
+      price: price
+    };
+    console.log('item', item);
+  };
+
   return (
     <li className={meal}>
       <div>
@@ -16,7 +27,7 @@ const MealItem = ({id, price, description, name}) => {
         <div className={priceStyle}>{formatPrice}원</div>
       </div>
       <div>
-        <MealItemForm id={id} />
+        <MealItemForm id={id} onAddToCart={addToCartHandler}/>
       </div>
     </li>
   );
--- src/components/Food/Meals/MealItem/MealItemForm.js
+++ src/components/Food/Meals/MealItem/MealItemForm.js
@@ -2,7 +2,7 @@ import React, { useRef, useState } from 'react';
 import Input from '../../../UI/Input/Input';
 import styles from './MealItemForm.module.scss';
 
-const MealItemForm = (props) => {
+const MealItemForm = ({ id, onAddToCart }) => {
 
   // 선택한 수량값
   // const [amount, setAmount] = useState(0);
@@ -12,26 +12,26 @@ const MealItemForm = (props) => {
 
   const submitHandler = e => {
     e.preventDefault();
-    const seletedValue = inputRef.current.value;
-    console.log('select: ', seletedValue);
+    const amount = inputRef.current.value;
+    // console.log('select: ', seletedValue);
+
+    onAddToCart(amount);
   };
 
   return (
     <form className={styles.form} onSubmit={submitHandler}>
-      {/* <Input label='취미' inputAttr={{type: 'checkbox', checked: 'checked', class: 'abc', id: 'aaa'}} /> */}
-      <input ref={inputRef} type='number' />
-      {/* <Input
+      <Input
         ref={inputRef}
         label='수량'
         inputAttr={{
-          id: 'amount_' + props.id,
+          id: 'amount_' + id,
           type: 'number',
           min: '1',
           max: '5',
           step: '1',
           defaultValue: '1',
         }}
-      /> */}
+      /> 
       <button>담기</button>
     </form>
   );
