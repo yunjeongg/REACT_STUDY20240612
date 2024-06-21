335214d1e896abf5be760d18376d573a829dd534
yunjeongg <bae.yunjeongg@gmail.com>
2024-06-21 09:51:37 +0900

    # 68. 기본저장 (Meals 관련 컴포넌트 생성)

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import Player from './components/Ref/Player';
 import TimerChallenge from './components/Ref/TimerChallenge';
 import Header from './components/Food/Layout/Header';
+import Meals from './components/Food/Meals/Meals';
 
 const App = () => {
 
   return (
     <>
       <Header />
+      <div id='main'>
+        <Meals />
+      </div>
     </>
   );
 };
