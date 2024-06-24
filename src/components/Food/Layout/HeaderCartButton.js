22f529d0719f17833a7435ec20cfa86d440fe79a
yunjeongg <bae.yunjeongg@gmail.com>
2024-06-24 10:23:56 +0900

    # 79. 장바구니 물건 총수량 배지 랜더링

--- src/components/Food/Layout/HeaderCartButton.js
+++ src/components/Food/Layout/HeaderCartButton.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import CartIcon from './CartIcon'
 import styles from './HeaderCartButton.module.scss';
+import CartContext from '../../../store/cart-context';
 
 const HeaderCartButton = ({ onShow }) => {
+
+  const { cartItems } = useContext(CartContext);
+
+  // 1번
+  // const calcTotalAmount = () => {
+  //   let totalAmount = 0;
+  //   for (const item of cartItems) {
+  //     totalAmount += item.amount;
+  //   }
+  // return totalAmount;
+
+  // 2번
+  const numberOfCart = cartItems.reduce((accum, current) => accum + current.amount, 0);
+
+
 
   const {button, icon, badge} = styles
   
@@ -12,7 +28,7 @@ const HeaderCartButton = ({ onShow }) => {
         <CartIcon />
       </span>
       <span>My Cart</span>
-      <span className={badge}>3</span>
+      <span className={badge}>{numberOfCart}</span>
     </button>
   )
 }
