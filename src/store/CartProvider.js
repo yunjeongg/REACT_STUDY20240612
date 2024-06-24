c9f8d7a2c91fc01a31b1ee7d6465c52f191cc188
yunjeongg <bae.yunjeongg@gmail.com>
2024-06-24 10:34:56 +0900

    # 80. 장바구니 중복수량 품목에 따른 수량증가

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -20,13 +20,30 @@ const cartReducer = (state, action) => {
   if (action.type === 'ADD') { // 장바구니 추가
     // 상태 업데이트 코드
     // 장바구니 배열 상태 업데이트
-    const updateCartItems = [...state.items, action.value];
+
+    // 장바구니에 추가될 신규 아이템
+    const newCartItem = action.value;
+
+    // 기존에 등록된 메뉴인지 확인하기 위해 해당 아이템의 인덱스를 탐색
+    const index = state.items.findIndex(item => item.id === newCartItem.id);
+
+    // 기존에 존재하는 아이템 배열의 사본
+    const existingItems = [...state.items];
+
+    // 신규 아이템인 경우
+    let updatedItems;
+    if (index === -1) {
+      updatedItems = [...existingItems, newCartItem];
+    } else { // 이미 장바구니에 있던 아이템의 추가 : 수량만 업데이트하기
+      existingItems[index].amount += newCartItem.amount;
+      updatedItems = [...existingItems];
+    }
 
     // 총액 상태 업데이트
     const updatePrice = state.totalPrice + (action.value.price * action.value.amount);
 
     return { // 새로운 상태
-      items: updateCartItems, // 기존것 + 새 아이템 붙이기
+      items: updatedItems, // 기존것 + 새 아이템 붙이기
       totalPrice: updatePrice
     };
   } else if (action.type === 'REMOVE') { // 장바구니 제거
