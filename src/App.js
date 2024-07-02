308eddd3768c02f29ba27ee204793274099dc67b
yunjeongg <bae.yunjeongg@gmail.com>
2024-07-02 11:28:33 +0900

    # 86. router 3 : 커스텀 에러페이지

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Products from './components/RouteExample/pages/Products';
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'; // 리액트에서 제공하는 라우터
 import RootLayout from './components/RouteExample/layout/RootLayout';
+import ErrorPage from './components/RouteExample/pages/ErrorPage';
 
 const router = createBrowserRouter
 ([
   {
     path: '/', 
     element: <RootLayout />, // / 들어오면 <RootLayout /> 을 띄우고
+    errorElement: <ErrorPage />, // 에러가 났을 때 보여줄 컴포넌트
     children: [
                 {path: '/', element: <Home /> }, // /products 들어오면 <Home /> 을 띄운다.
                 {path: '/products', element: <Products /> } // /products 들어오면 <Products /> 을 띄운다.
