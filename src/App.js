e13ba1cb99a6511b94842aa16b4a6925385cbc8b
yunjeongg <bae.yunjeongg@gmail.com>
2024-07-02 11:49:55 +0900

    # 88. router 5 : 상대경로로 링크 제어하기

--- src/App.js
+++ src/App.js
@@ -9,12 +9,20 @@ import ErrorPage from './components/RouteExample/pages/ErrorPage';
 const router = createBrowserRouter
 ([
   {
-    path: '/', 
+    path: '/abc', 
     element: <RootLayout />, // / 들어오면 <RootLayout /> 을 띄우고
     errorElement: <ErrorPage />, // 에러가 났을 때 보여줄 컴포넌트
     children: [
-                {path: '/', element: <Home /> }, // /products 들어오면 <Home /> 을 띄운다.
-                {path: '/products', element: <Products /> } // /products 들어오면 <Products /> 을 띄운다.
+                // {path: '', element: <Home /> }, // /products 들어오면 <Home /> 을 띄운다.
+                {index: true, element: <Home /> }, 
+                // 웹페이지에 들어왔을 때 맨 처음 보여지는 부분이라 path: '' 이런식으로비워두는 것보다 index: true 이런식으로 작성하는게 좋다.
+                {path: 'products', element: <Products /> } // /products 들어오면 <Products /> 을 띄운다.
+
+      // children 의 path 는 전체 path 보다 더 상위의 path가 올 수 없고
+      // children 의 path 는 전체 path + 자기path 로 작성해야된다.
+
+      // childe path 에서 전체path 를 생략할 수 있다. 
+      // /base -> '', /base/product -> 'products'
   ]},
 ]);
 const App = () => {
