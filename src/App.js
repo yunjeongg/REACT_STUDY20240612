b6450813f5926eb8bf6d746a22e4211d3f9a7f98
yunjeongg <bae.yunjeongg@gmail.com>
2024-07-02 12:11:24 +0900

    # 89. router 6 : 동적라우팅, 라우터로 파라미터 얻기

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Products from './components/RouteExample/pages/Products';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'; // 리액트에서 제공하는 라우터
 import RootLayout from './components/RouteExample/layout/RootLayout';
 import ErrorPage from './components/RouteExample/pages/ErrorPage';
+import ProductDetail from './components/RouteExample/pages/ProductDetail';
 
 const router = createBrowserRouter
 ([
@@ -16,7 +17,12 @@ const router = createBrowserRouter
                 // {path: '', element: <Home /> }, // /products 들어오면 <Home /> 을 띄운다.
                 {index: true, element: <Home /> }, 
                 // 웹페이지에 들어왔을 때 맨 처음 보여지는 부분이라 path: '' 이런식으로비워두는 것보다 index: true 이런식으로 작성하는게 좋다.
-                {path: 'products', element: <Products /> } // /products 들어오면 <Products /> 을 띄운다.
+                {path: 'products', element: <Products /> }, // /products 들어오면 <Products /> 을 띄운다.
+                // {path: 'products/:prodId', element: <ProductDetail /> } 
+                // : 붙이는 것 - 동적렌더링
+                {path: 'products/:prodId/page/:pageNo', element: <ProductDetail /> } 
+                // 여러가지 파라미터를 한번에 알아낼수도 있다. (상품의 아이디, 페이지번호)
+
 
       // children 의 path 는 전체 path 보다 더 상위의 path가 올 수 없고
       // children 의 path 는 전체 path + 자기path 로 작성해야된다.
--- src/components/RouteExample/pages/Products.js
+++ src/components/RouteExample/pages/Products.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 
+const DUMMY_PRODUCTS = 
+[
+  { id: 'p1', name: '세탁기'},
+  { id: 'p2', name: '에어컨'},
+  { id: 'p3', name: '청소기'},
+]
 const Products = () => {
 
   console.log("Products");
@@ -8,9 +14,15 @@ const Products = () => {
   return (
     <>
       <h1>My Products Page</h1>
-      <p>
-        <Link to='..'>Home</Link> 페이지로 이동하기
-      </p>
+      <ul>
+        {
+          DUMMY_PRODUCTS.map(prod => (
+            <li key={prod.id}>
+              <Link to={`${prod.id}/page/10`}>{prod.name}</Link>
+            </li>
+          ))
+        }
+      </ul>
     </>
   )
 }
