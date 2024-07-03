961c1c32245835e888c5a102c508cd0d4fa5a1fa
yunjeongg <bae.yunjeongg@gmail.com>
2024-07-03 10:02:24 +0900

    # 93. Events 서버에서 데이터 GET으로 가져오기

--- src/components/RouteExample/pages/Events.js
+++ src/components/RouteExample/pages/Events.js
@@ -42,6 +42,18 @@ const Events = () => {
   const [eventList, setEventList] = useState([]);
 
   useEffect(() => {
+    // useEffect 에 직접적으로 async 를 적용할 수 없고, useEffec 안에 async 함수 따로 만들어야한다. 
+    (async () => {
+      const response = await fetch('http://localhost:8282/events');
+      const jsonData = await response.json();
+      setEventList(jsonData);
+    })();
+  ;
+  }, []);
+
+  /*
+  useEffect(() => {
+
     fetch('http://localhost:8282/events')
     .then(res => res.json())
     .then(jsonData => {
@@ -49,6 +61,8 @@ const Events = () => {
       setEventList(jsonData); // 상태변수에 jsonData 추가 (상태변수 변경)
   })
   }, []); // useEffect 의 [] 안에 값을 넣으면, []변경이 일어날 때 마다 변경이 일어나고, []를 비워두면 단 한번만 변경이 일어남
+
+  */
 
   return (
     <>
