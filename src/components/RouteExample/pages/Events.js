27a8c78e6673e92b0c256be8963901caaba797ff
yunjeongg <bae.yunjeongg@gmail.com>
2024-07-10 09:46:37 +0900

    # 113. IntersectionObserver 을 사용한 무한스크롤

--- src/components/RouteExample/pages/Events.js
+++ src/components/RouteExample/pages/Events.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import EventList from '../components/EventList';
 import { useLoaderData, json } from 'react-router-dom';
 import EventSkeleton from '../components/EventSkeleton';
@@ -10,6 +10,9 @@ const Events = () => {
   // loader가 리턴한 데이터 받아오기
   // const eventList = useLoaderData();
   // console.log(eventList);
+
+  // 이벤트 목록 아래 박스 참조
+  const skeletonBoxRef = useRef();
 
   // 서버에서 가져온 이벤트 목록
   const [events, setEvents] = useState([]);
@@ -70,10 +73,11 @@ const Events = () => {
 
   };
 
-  // 초기 이벤트 1페이지 목록 가져오기
-  useEffect(() => {
-    loadEvents();
-  }, []);
+  // // 초기 이벤트 1페이지 목록 가져오기
+  // 처음에 노란박스가 보이기 때문에 필요없음
+  // useEffect(() => {
+  //   loadEvents();
+  // }, []);
 
   // 스크롤 핸들러
   // // 1. 디바운싱 (스크롤 멈춘 후 일정 초(2초) 이후 재로딩)
@@ -82,32 +86,72 @@ const Events = () => {
   // }, 2000);
 
   // 2. 스로틀링 (스크롤을 돌리던 말던 일정시간에 한번씩 재로딩)
-  const scrollHandler = throttle(() => {
-    // console.log('scroll');
+    // 처음에 노란박스가 보이기 때문에 필요없음
+  // const scrollHandler = throttle(() => {
+  //   // console.log('scroll');
 
-    // 로딩중이거나, 화면의 맨 밑바닥이라면 return
-    if(loading || window.innerHeight + document.documentElement.scrollTop >= document.documentElement.offsetHeight) {
-      return;
+  //   // 로딩중이거나, 화면의 맨 밑바닥이라면 return
+  //   // 아래 공식은 화면 하단부를 감지하는 로직 (만약 게시물의 height 가 하단부에 닿지 않는 경우 스크롤이벤트가 작동하지 않는다.)
+  //   // 해결방법 - 화면의 하단에 가상의 박스를 설치하고 게시물의 밑면이 박스에 닿는 경우 스크롤이벤트를 작동시키기.
+  //   if(loading || window.innerHeight + document.documentElement.scrollTop >= document.documentElement.offsetHeight) {
+  //     return;
+  //   }
+  //   loadEvents();
+  // }, 2000);
+
+  // // 스로틀링
+  // // 처음한번만실행되도록 useEffect
+  // // 처음에 노란박스가 보이기 때문에 필요없음
+  // useEffect(()=> {
+  //   window.addEventListener('scroll', scrollHandler);
+
+  //   // 이벤트 끝난 후 이벤트를 지워줘야한다.
+  //   return () => {
+  //     window.removeEventListener('scroll', scrollHandler);
+  //     scrollHandler.cancel(); // 스로틀 취소
+  //   }
+  // }, [currentPage, loading]); // 밑바닥이 아니면 다시 실행
+
+  // 화면에 특정 박스가 보이면 다음 페이지를 로딩
+  useEffect (() => {
+    const observer = new IntersectionObserver((entries) => { // 바닐라js에서도 사용가능
+
+      // entries 는 현재 감시하고 있는 대상의 target 의 정보를 알려줌 (배열)
+      console.log('entries', entries[0]); // isIntersecting 가 true이면 노란박스 보이는 상태, false 면 보이지 않는 상태
+      
+      // 서버에서 데이터 페칭
+      // !entries[0].isIntersecting : 관찰하고 있는 박스가 감지되지 않으면
+      if (!entries[0].isIntersecting || loading || isFinish) { // 로딩중이거나 다 가져왔을 경우 옵저빙 중지
+        return;
+      }
+      loadEvents();
+    }, {
+      // 관찰하고 있는 요소의 높이가 50%보일 때 콜백을 실행한다.
+      threshold: 0.5
+    }); 
+
+    // observer 관찰 대상 (DOM) 을 지정하기
+    if (skeletonBoxRef.current) {
+      observer.observe(skeletonBoxRef.current);
     }
-    loadEvents();
-  }, 2000);
 
-  // 스로틀링
-  // 처음한번만실행되도록 useEffect
-  useEffect(()=> {
-    window.addEventListener('scroll', scrollHandler);
-
-    // 이벤트 끝난 후 이벤트를 지워줘야한다.
+    // 컴포넌트가 랜더링이 사라질 때 옵저빙 중지
     return () => {
-      window.removeEventListener('scroll', scrollHandler);
-      scrollHandler.cancel(); // 스로틀 취소
-    }
-  }, [currentPage, loading]); // 밑바닥이 아니면 다시 실행
+      if (skeletonBoxRef.current) {
+        observer.disconnect();
+      }
+    };
+
+  }, [loading, currentPage]);
 
   return (
     <>
       <EventList eventList={events} />
-      {loading && <EventSkeleton count={skeletonCount} />}
+      <div ref={skeletonBoxRef} 
+                                // style={{height: '300px', background: ' yellow'}}
+                                >
+        {loading && <EventSkeleton count={skeletonCount} />}
+      </div>
     </>
   );
 };
